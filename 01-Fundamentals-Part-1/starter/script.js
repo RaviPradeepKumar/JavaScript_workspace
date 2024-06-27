b05eea2afd17e8001b6ef11b82c342f96dd261eb
RAAVI PRADEEP KUMAR <pradeep.ravi@dreamstep.in>
2024-06-27 23:38:38 +0530

    values and variables and also assignment

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,46 @@
+/*let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN!');
+console.log(40 + 23 + 8 - 10);
+*/
+/* values and variables
+=======================
+console.log('jonas');
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// let 3years = 3;
+
+let jonas_matilda = 'JM';
+
+let $function = 23;
+
+// let name = "Jonas";
+
+// let Person = "Jonas";
+
+let PI = 3.1415;
+
+let myFirstJob = "coder";
+let myCurrentJob = "teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+*/
+/* Assignment
+===============
+const country = "india";
+const continent = "asia";
+const population = "60 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log(country, continent, population);
+*/
